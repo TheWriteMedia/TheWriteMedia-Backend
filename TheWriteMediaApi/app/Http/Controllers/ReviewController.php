7926407792d81ecc579bd69bb3d468911b332c90
Maylse <milessabal123@gmail.com>
2025-03-11 04:24:40 +0800

    Dockerfile testing

--- TheWriteMediaApi/app/Http/Controllers/ReviewController.php
+++ TheWriteMediaApi/app/Http/Controllers/ReviewController.php
@@ -11,19 +11,12 @@ class ReviewController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index()
     {
-        // Get the currently authenticated user
-    $user = $request->user();
-
-    // Check the user type
-    if ($user->user_type === User::USER_TYPE_WEB_ADMIN) {
-        // If the user is a web_admin, show all reviews (active and inactive)
-        $reviews = Review::with('user')->latest()->get();
-    } else {
-        // If the user is an author, show only active reviews
-        $reviews = Review::with('user')->where('status', 'ACTIVE')->latest()->get();
-    }
+   
+   
+        $reviews = Review::with('book')->latest()->get();
+    
 
     return response()->json([
         'reviews' => $reviews
@@ -36,11 +29,13 @@ class ReviewController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'book_id' => 'required|string|max:1000',
             'rating' => 'required|integer|min:1|max:5',
             'review_message' => 'required|string|max:1000',
         ]);
 
         $review = Review::create([
+            'book_id' => $request->book_id,
             'rating' => $request->rating,
             'review_message' => $request->review_message,
             'status' => 'PENDING', // Default status
